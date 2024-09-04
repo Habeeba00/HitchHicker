53e8a2984d68142ce38d41ee050b747c54a586d7
Habeeba00 <habibamohamed2222009u665@gmail.com>
2024-09-04 16:37:03 +0300

    UserEcxceptions

--- hitchhiker/CustomUser/serializers.py
+++ hitchhiker/CustomUser/serializers.py
@@ -20,56 +20,44 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class SignUpSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
-    username=serializers.CharField(required=True)
-
+    
+    username = serializers.CharField(
+        required=True, 
+        validators=[UniqueValidator(queryset=CustomUser.objects.all())]
+    )
+    
     password = serializers.CharField(
-        write_only=True, required=True, validators=[validate_password])
+        write_only=True, required=True, validators=[validate_password]
+    )
     confirm_password = serializers.CharField(write_only=True, required=True)
     email = serializers.EmailField(
         required=True,
-        validators=[UniqueValidator(queryset=CustomUser.objects.all())])
+        validators=[UniqueValidator(queryset=CustomUser.objects.all())]
+    )
     phone = serializers.IntegerField(required=True)
 
     class Meta:
         model = CustomUser
-        fields=['id','first_name','last_name','username','email','phone','password','confirm_password']
-
-    def validate_username(self, value):
-        if CustomUser.objects.filter(username=value).exists():
-            raise serializers.ValidationError("Username already exists.")
-        return value
+        fields = ['id', 'first_name', 'last_name', 'username', 'email', 'phone', 'password', 'confirm_password']
 
     def validate(self, attrs):
         if attrs['password'] != attrs['confirm_password']:
             raise serializers.ValidationError(
                 {"password": "Password fields didn't match."})
-
         return attrs
-
+    
     def create(self, validated_data):
+        # Remove the confirm_password key-value pair
+        validated_data.pop('confirm_password')
+
         user = CustomUser.objects.create(
             username=validated_data['username'],
             email=validated_data['email'],
             phone=validated_data['phone'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
-
-
         )
 
         user.set_password(validated_data['password'])
         user.save()
-
         return user
-    
-
-
-# class SignInSerializer(serializers.Serializer):
-#     username=serializers.CharField(required=True)
-#     password=serializers.CharField(style={'input_type':"password"})
-
-
-#     class Meta:
-#         model=CustomUser
-#         fields=['username','password']
-    
--- hitchhiker/CustomUser/views.py
+++ hitchhiker/CustomUser/views.py
@@ -1,5 +1,3 @@
-
-
 from rest_framework import generics,status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny 
@@ -20,20 +18,14 @@ class SignUpView(generics.ListCreateAPIView):
 
 
     def create(self, request, *args, **kwargs):
-        # Call the parent class to create the user
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         print(user.is_active)
-        # You can add any custom data to the response here if needed
         response_data = {
-            'user': serializer.data,  # This will return the serialized data of the created user
+            'user': serializer.data,
             'message': 'User registered successfully'
         }
 
         return Response(response_data, status=status.HTTP_201_CREATED)
-    
-
-
-
     
