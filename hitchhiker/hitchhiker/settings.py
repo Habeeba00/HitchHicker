deddac9be3005d498018e396c504012dd231f593
Habeeba00 <habibamohamed2222009u665@gmail.com>
2024-09-05 17:17:28 +0300

    otp not working

--- hitchhiker/hitchhiker/settings.py
+++ hitchhiker/hitchhiker/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-x0xy8zgvugk76ku^*ers92$1)l)k^9!k1%b=*2ab3y8*!#p$0*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS =['127.0.0.1', 'localhost']
+ALLOWED_HOSTS =['*']
+
 
 
 # Application definition
